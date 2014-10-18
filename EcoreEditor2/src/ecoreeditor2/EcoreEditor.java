7045a0de08360ab3154ad10b58eee17e23cba2cc
ClemensElflein <clemens1@familie-elflein.de>
2014-10-18 11:32:12 +0200

    WIP

--- EcoreEditor2/src/ecoreeditor2/EcoreEditor.java
+++ EcoreEditor2/src/ecoreeditor2/EcoreEditor.java
@@ -1,16 +1,45 @@
 package ecoreeditor2;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.common.command.CommandStack;
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecp.view.model.common.edit.provider.CustomReflectiveItemProviderAdapterFactory;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.internal.commands.CommandStateProxy;
 import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class EcoreEditor extends EditorPart {
 
+	// The Resource loaded from the provided EditorInput
+	private Resource resource;
+	
+	// Use a simple CommandStack that can undo and redo nothing.
+	private CommandStack commandStack = new BasicCommandStack();
+	
 	public EcoreEditor() {
 		// TODO Auto-generated constructor stub
 	}
@@ -51,8 +80,55 @@ public class EcoreEditor extends EditorPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		new Label(parent, SWT.None).setText("This is a Test");
+		loadResource();
+		
+		EPackage ePackage = (EPackage) resource.getContents().get(0);
+		
+	}
+	
+	/*
+	 * Get
+	 */
+	
+	/*
+	 * Loads the Resource from the EditorInput and sets the EcoreEditor.resource field.
+	 */
+	private void loadResource() {
+		final FileEditorInput fei = (FileEditorInput) getEditorInput();
+		final ResourceSet resourceSet = createResourceSet();
+		try {
+			final Map<Object, Object> loadOptions = new HashMap<Object, Object>();
+			loadOptions
+				.put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
 
+			resource = resourceSet.createResource(URI.createURI(fei.getURI().toURL().toExternalForm()));
+			resource.load(loadOptions);
+
+			// resolve all proxies
+			int rsSize = resourceSet.getResources().size();
+			EcoreUtil.resolveAll(resourceSet);
+			while (rsSize != resourceSet.getResources().size()) {
+				EcoreUtil.resolveAll(resourceSet);
+				rsSize = resourceSet.getResources().size();
+			}
+
+		} catch (final IOException e) {
+			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e));
+		}
+	}
+	
+	private ResourceSet createResourceSet() {
+		final ResourceSet resourceSet = new ResourceSetImpl();
+
+		final AdapterFactoryEditingDomain domain = new AdapterFactoryEditingDomain(
+			new ComposedAdapterFactory(
+					new AdapterFactory[] {
+							new CustomReflectiveItemProviderAdapterFactory(),
+							new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE)
+							}),
+					commandStack, resourceSet);
+		resourceSet.eAdapters().add(new AdapterFactoryEditingDomain.EditingDomainProvider(domain));
+		return resourceSet;
 	}
 
 	@Override
