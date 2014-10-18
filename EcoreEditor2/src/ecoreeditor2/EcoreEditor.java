b3417be12d3c4442e35c129a570418493ee4a1eb
Clemens <clemens.elflein@googlemail.com>
2014-10-18 12:50:30 +0200

    Simple Editor now working

--- EcoreEditor2/src/ecoreeditor2/EcoreEditor.java
+++ EcoreEditor2/src/ecoreeditor2/EcoreEditor.java
@@ -1,7 +1,7 @@
 package ecoreeditor2;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.EventObject;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,26 +9,24 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.BasicCommandStack;
-import org.eclipse.emf.common.command.CommandStack;
+import org.eclipse.emf.common.command.CommandStackListener;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecp.ui.view.ECPRendererException;
+import org.eclipse.emf.ecp.ui.view.swt.ECPSWTViewRenderer;
 import org.eclipse.emf.ecp.view.model.common.edit.provider.CustomReflectiveItemProviderAdapterFactory;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.internal.commands.CommandStateProxy;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.ui.part.FileEditorInput;
 
@@ -38,7 +36,7 @@ public class EcoreEditor extends EditorPart {
 	private Resource resource;
 	
 	// Use a simple CommandStack that can undo and redo nothing.
-	private CommandStack commandStack = new BasicCommandStack();
+	private BasicCommandStack commandStack = new BasicCommandStack();
 	
 	public EcoreEditor() {
 		// TODO Auto-generated constructor stub
@@ -46,8 +44,16 @@ public class EcoreEditor extends EditorPart {
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-
+		try {
+			resource.save(null);
+			
+			// Tell the CommandStack, that we have saved the file successfully
+			// and inform the Workspace, that the Dirty property has changed.
+			commandStack.saveIsDone();
+			firePropertyChange(PROP_DIRTY);
+		} catch (IOException e) {
+			Log.e(e);
+		}
 	}
 
 	@Override
@@ -56,25 +62,32 @@ public class EcoreEditor extends EditorPart {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.part.EditorPart#init(org.eclipse.ui.IEditorSite, org.eclipse.ui.IEditorInput)
-	 */
 	@Override
 	public void init(IEditorSite site, IEditorInput input)
 			throws PartInitException {
 		setSite(site);
 		setInput(input);
+		
+		// Set the Title for this Editor to the Name of the Input (= Filename)
+		setPartName(input.getName());
+		
+		// As soon as the resource changed, we inform the Workspace, that it is now dirty
+		commandStack.addCommandStackListener(new CommandStackListener() {
+			@Override
+			public void commandStackChanged(EventObject event) {
+				EcoreEditor.this.firePropertyChange(PROP_DIRTY);
+			}
+		});
 	}
 
 	@Override
 	public boolean isDirty() {
-		// TODO Auto-generated method stub
-		return false;
+		return commandStack.isSaveNeeded();
 	}
 
 	@Override
 	public boolean isSaveAsAllowed() {
-		// TODO Auto-generated method stub
+		// We don't allow the SaveAs operation at this time.
 		return false;
 	}
 
@@ -83,12 +96,12 @@ public class EcoreEditor extends EditorPart {
 		loadResource();
 		
 		EPackage ePackage = (EPackage) resource.getContents().get(0);
-		
+		try {
+			ECPSWTViewRenderer.INSTANCE.render(parent, ePackage);
+		} catch (final ECPRendererException ex) {
+			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, ex.getMessage(), ex));
+		}
 	}
-	
-	/*
-	 * Get
-	 */
 	
 	/*
 	 * Loads the Resource from the EditorInput and sets the EcoreEditor.resource field.
@@ -133,8 +146,6 @@ public class EcoreEditor extends EditorPart {
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
-
+		// NOP
 	}
-
 }
