15dc01ece7bdc489eed38784ddead0a1889b72e4
Clemens <clemens.elflein@googlemail.com>
2014-11-03 16:37:39 +0100

    Added test Projects

---
+++ org.eclipse.emf.ecp.ecoreeditor.ecore.test/src/org/eclipse/emf/ecp/ecoreeditor/ecore/test/controls/DataTypeControlTest.java
@@ -0,0 +1,34 @@
+package org.eclipse.emf.ecp.ecoreeditor.ecore.test.controls;
+
+import static org.junit.Assert.fail;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecp.ecoreeditor.ecore.controls.DataTypeControl;
+import org.eclipse.emf.ecp.ui.view.ECPRendererException;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataTypeControlTest {
+
+	private Shell shell;
+	private EClass object;
+	private DataTypeControl control;
+
+	@Before
+	public void init() throws ECPRendererException {
+		shell = new Shell(Display.getDefault());
+
+		object = EcoreFactory.eINSTANCE.createEClass();
+		object.getEStructuralFeatures().add(
+				EcorePackage.eINSTANCE.getETypedElement_EType());
+	}
+
+	@Test
+	public void testDatabindingWorks() {
+		fail("Not implemented yet");
+	}
+}
