4a6dce16d50bc0417366918fe9e0eed39f42c21f
Clemens <clemens.elflein@googlemail.com>
2014-12-08 13:03:01 +0100

    Added .gitignore files to ignore target directories

---
+++ org.eclipse.emf.ecp.ecoreeditor/src/org/eclipse/emf/ecp/ecoreeditor/EcoreEditorActionBarContributor.java
@@ -0,0 +1,7 @@
+package org.eclipse.emf.ecp.ecoreeditor;
+
+import org.eclipse.emf.edit.ui.action.EditingDomainActionBarContributor;
+
+public class EcoreEditorActionBarContributor extends
+		EditingDomainActionBarContributor {
+}
